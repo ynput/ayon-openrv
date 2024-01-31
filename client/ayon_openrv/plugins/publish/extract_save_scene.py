fc4d101675f626550531e4f4790a227f54ba740b
Petr Kalis <petr.kalis@gmail.com>
2024-01-31 17:32:45 +0100

    Added plugin to save workfile before extraction

---
+++ client/ayon_openrv/plugins/publish/extract_save_scene.py
@@ -0,0 +1,21 @@
+import pyblish.api
+from openpype.pipeline import registered_host
+from openpype.pipeline import publish, KnownPublishError
+
+
+class ExtractSaveScene(pyblish.api.ContextPlugin):
+    """Save scene before extraction."""
+
+    order = publish.Extractor.order - 0.48
+    label = "Extract Save Scene"
+    hosts = ["openrv"]
+
+    def process(self, context):
+        host = registered_host()
+
+        current_file_name = host.get_current_workfile()
+        self.log.info("current_file_name::{}".format(current_file_name))
+        if not current_file_name:
+            raise KnownPublishError("Scene not saved, use Workfile app "
+                                    "to save first!")
+        host.save_workfile(current_file_name)
