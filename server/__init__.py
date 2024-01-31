a115a76616d2f525f70c085aaf01b298f7b038c7
Petr Kalis <petr.kalis@gmail.com>
2024-01-31 17:00:40 +0100

    Updated main Setting
    Addon itself doesn't have any settings, executable is set in Applications.

--- server/__init__.py
+++ server/__init__.py
@@ -2,15 +2,15 @@ from typing import Type
 
 from ayon_server.addons import BaseServerAddon
 
-from .settings import MySettings, DEFAULT_VALUES
+from .settings import OpenRVSettings, DEFAULT_VALUES
 from .version import __version__
 
 
-class MyAddon(BaseServerAddon):
-    name = ""
-    title = ""
+class OpenRVAddon(BaseServerAddon):
+    name = "openrv"
+    title = "OpenRV"
     version = __version__
-    settings_model: Type[MySettings] = MySettings
+    settings_model: Type[OpenRVSettings] = OpenRVSettings
 
     async def get_default_settings(self):
         settings_model_cls = self.get_settings_model()
---
+++ server/settings/__init__.py
@@ -0,0 +1,10 @@
+from .main import (
+    OpenRVSettings,
+    DEFAULT_VALUES
+)
+
+
+__all__ = (
+    "OpenRVSettings",
+    "DEFAULT_VALUES"
+)
