a1696772c6a1a678941ea2b44f5495847a006106
Petr Kalis <petr.kalis@gmail.com>
2024-01-31 17:33:18 +0100

    Added plugin to version up workfile after publishing

---
+++ client/ayon_openrv/plugins/publish/increment_workfile.py
@@ -0,0 +1,30 @@
+import pyblish.api
+from openpype.lib import version_up
+from openpype.pipeline import registered_host
+from openpype.pipeline.publish import get_errored_plugins_from_context
+
+
+class IncrementWorkfile(pyblish.api.InstancePlugin):
+    """Increment the current workfile.
+
+    Saves the current scene with an increased version number.
+    """
+
+    label = "Increment Workfile"
+    order = pyblish.api.IntegratorOrder + 9.0
+    hosts = ["openrv"]
+    families = ["workfile"]
+    optional = True
+
+    def process(self, instance):
+        errored_plugins = get_errored_plugins_from_context(instance.context)
+        if errored_plugins:
+            raise RuntimeError(
+                "Skipping incrementing current file because publishing failed."
+            )
+
+        scene_path = version_up(instance.context.data["currentFile"])
+        host = registered_host()
+        host.save_workfile(scene_path)
+
+        self.log.info("Incremented workfile to: {}".format(scene_path))
