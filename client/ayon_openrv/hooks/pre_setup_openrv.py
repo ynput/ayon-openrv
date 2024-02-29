f51234ec10ef2783a3820fb03ff8002de66e9b1d
Tony Dorfmeister <tony.dorfmeister@bepic.studio>
2024-02-29 15:15:59 +0100

    useless lol

--- client/ayon_openrv/hooks/pre_setup_openrv.py
+++ client/ayon_openrv/hooks/pre_setup_openrv.py
@@ -60,34 +60,3 @@ class PreSetupOpenRV(PreLaunchHook):
             support_path = str(op_support_path)
         self.log.debug(f"Setting RV_SUPPORT_PATH: {support_path}")
         self.launch_context.env["RV_SUPPORT_PATH"] = support_path
-
-        # set $OCIO
-        template_data = get_template_data_with_names(
-            project_name=self.data["project_name"],
-            asset_name=self.data["folder_path"],
-            task_name=self.data["task_name"],
-            host_name=self.host_name,
-            settings=self.data["project_settings"]
-        )
-
-        config_data = get_imageio_config(
-            project_name=self.data["project_name"],
-            host_name=self.host_name,
-            project_settings=self.data["project_settings"],
-            anatomy_data=template_data,
-            anatomy=self.data["anatomy"],
-            env=self.launch_context.env,
-        )
-
-        if config_data:
-            ocio_path = config_data["path"]
-
-            if self.host_name in ["nuke", "hiero"]:
-                ocio_path = ocio_path.replace("\\", "/")
-
-            self.log.info(
-                f"Setting OCIO environment to config path: {ocio_path}")
-
-            self.launch_context.env["OCIO"] = ocio_path
-        else:
-            self.log.debug("OCIO not set or enabled")
