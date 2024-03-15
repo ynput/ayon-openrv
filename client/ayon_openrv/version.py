264013dab65d20b5c71f7df43389d0be48cc446c
Petr Kalis <petr.kalis@gmail.com>
2024-03-15 17:15:32 +0100

    Updated create_package to follow Rez approach

--- client/ayon_openrv/version.py
+++ client/ayon_openrv/version.py
@@ -1,1 +1,3 @@
+# -*- coding: utf-8 -*-
+"""Package declaring openrv addon version."""
 __version__ = "1.0.0"
--- version.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Package declaring addon version."""
-__version__ = "1.0.0"
