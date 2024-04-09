3fe8d02be6471e68a5b3bf758b12d5d7db9bfe8c
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-04-09 15:17:04 +0200

    use 'ExtractorOrder' instead of order from 'Extractor'

--- client/ayon_openrv/plugins/publish/extract_save_scene.py
+++ client/ayon_openrv/plugins/publish/extract_save_scene.py
@@ -6,7 +6,7 @@ from openpype.pipeline import publish, KnownPublishError
 class ExtractSaveScene(pyblish.api.ContextPlugin):
     """Save scene before extraction."""
 
-    order = publish.Extractor.order - 0.48
+    order = pyblish.api.ExtractorOrder - 0.48
     label = "Extract Save Scene"
     hosts = ["openrv"]
 
