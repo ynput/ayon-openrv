8ac63caee81f3f71685ab9e996fbfd47c974923d
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-07-15 13:30:34 +0200

    add '__version__' to init file

--- client/ayon_openrv/__init__.py
+++ client/ayon_openrv/__init__.py
@@ -1,3 +1,4 @@
+from .version import __version__
 from .addon import (
     OpenRVAddon,
     OPENRV_ROOT_DIR
@@ -5,6 +6,8 @@ from .addon import (
 
 
 __all__ = (
+    "__version__",
+
     "OpenRVAddon",
     "OPENRV_ROOT_DIR"
 )
