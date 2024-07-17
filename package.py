d6ecc7ea4938f33ebc955b1ca9ffbf0773865a73
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-07-17 10:41:27 +0200

    use ge

--- package.py
+++ package.py
@@ -7,6 +7,6 @@ client_dir = "ayon_openrv"
 plugin_for = ["ayon_server"]
 build_command = ""
 ayon_required_addons = {
-    "core": ">0.3.0",
+    "core": ">=0.3.0",
     "applications": ">=0.2.0",
 }
