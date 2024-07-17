63b3787aba3a37551feabdc516e838bb7026c52c
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-07-17 11:14:37 +0200

    removed unnecessary f-strings

--- client/ayon_openrv/plugins/load/global/play_in_rv.py
+++ client/ayon_openrv/plugins/load/global/play_in_rv.py
@@ -45,10 +45,10 @@ class PlayInRV(load.LoaderPlugin):
             )
             if not openrv_app:
                 raise LoadError(
-                    f"No configured OpenRV found in"
-                    f" Applications. Ask admin to configure it"
-                    f" in ayon+settings://applications/applications/openrv."
-                    f"\nProvide '-network' there as argument."
+                    "No configured OpenRV found in"
+                    " Applications. Ask admin to configure it"
+                    " in ayon+settings://applications/applications/openrv."
+                    "\nProvide '-network' there as argument."
                 )
             openrv_app.launch(
                 project_name=project_name,
