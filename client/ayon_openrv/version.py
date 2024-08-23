302a5706ecde9d340665bae6693ac38393cdce19
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-08-23 11:17:32 +0200

    bump version to '1.0.1'

--- client/ayon_openrv/version.py
+++ client/ayon_openrv/version.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
-"""Package declaring openrv addon version."""
-__version__ = "1.0.1-dev.1"
+"""Package declaring AYON addon 'openrv' version."""
+__version__ = "1.0.1"
--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 name = "openrv"
 title = "OpenRV"
-version = "1.0.1-dev.2"
+version = "1.0.1"
 
 client_dir = "ayon_openrv"
 
