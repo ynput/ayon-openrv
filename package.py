91456d05f6b6c21732a7acda6395c6b974dcc42a
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-10-16 11:01:08 +0200

    define app host name

--- package.py
+++ package.py
@@ -1,12 +1,12 @@
 name = "openrv"
 title = "OpenRV"
 version = "1.0.2-dev.1"
-
+app_host_name = "openrv"
 client_dir = "ayon_openrv"
 
-plugin_for = ["ayon_server"]
-build_command = ""
+ayon_server_version = ">=1.1.2"
 ayon_required_addons = {
     "core": ">=0.3.0",
     "applications": ">=0.2.0",
 }
+ayon_compatible_addons = {}
